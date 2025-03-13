f1f352f16e98a2671bbe1a33af61c33f28e27ce8
Logan Silvers <silvers@colum.edu>
2025-03-13 17:15:25 -0500

    2048 update 1

--- galaxypets/galaxypets-site/2048/index.js
+++ galaxypets/galaxypets-site/2048/index.js
@@ -8,13 +8,6 @@ window.onload = function() {
 }
 
 function setGame() {
-    // board = [
-    //     [2, 2, 2, 2],
-    //     [2, 2, 2, 2],
-    //     [4, 4, 8, 8],
-    //     [4, 4, 8, 8]
-    // ];
-
     board = [
         [0, 0, 0, 0],
         [0, 0, 0, 0],
@@ -77,20 +70,20 @@ function filterZero(row){
 }
 
 function slide(row) {
-    //[0, 2, 2, 2] 
-    row = filterZero(row); //[2, 2, 2]
+
+    row = filterZero(row);
     for (let i = 0; i < row.length-1; i++){
         if (row[i] == row[i+1]) {
             row[i] *= 2;
             row[i+1] = 0;
             score += row[i];
         }
-    } //[4, 0, 2]
-    row = filterZero(row); //[4, 2]
-    //add zeroes
+    } 
+    row = filterZero(row); 
+
     while (row.length < columns) {
         row.push(0);
-    } //[4, 2, 0, 0]
+    } 
     return row;
 }
 
@@ -109,10 +102,10 @@ function slideLeft() {
 
 function slideRight() {
     for (let r = 0; r < rows; r++) {
-        let row = board[r];         //[0, 2, 2, 2]
-        row.reverse();              //[2, 2, 2, 0]
-        row = slide(row)            //[4, 2, 0, 0]
-        board[r] = row.reverse();   //[0, 0, 2, 4];
+        let row = board[r];        
+        row.reverse();             
+        row = slide(row)            
+        board[r] = row.reverse();   
         for (let c = 0; c < columns; c++){
             let tile = document.getElementById(r.toString() + "-" + c.toString());
             let num = board[r][c];
