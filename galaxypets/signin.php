c5a20a78db4e61baee5bed465589e422a574052e
Marina Atkin <marinaatkin@gmail.com>
2025-03-20 14:20:26 -0500

    sign in page

--- galaxypets/signin.php
+++ galaxypets/signin.php
@@ -143,7 +143,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a></p>
+            <p>Don't have an account? <a href="register.php">Sign up now</a> or <a href="loginAsGuest.php">login as a guest</a>.</p>
             <p><a href="ForgotMyPassword.php">I forgot my password</a></p>
         </form>
     </div>
