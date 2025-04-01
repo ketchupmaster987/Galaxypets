e3249ec9336f6a087271ef09b05e64ebfce5beaa
logan <Loganold@hotmail.com>
2025-04-01 14:06:40 -0400

    smile

--- index.php
+++ index.php
@@ -5,32 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GalaxyPets - Your Space Adventure Begins!</title>
     <link rel="stylesheet" href="Assets/css/style.css">
-    <link rel="preload" href="Assets/img/aquagalaxy_small.gif" as="image">
+    <link rel="stylesheet" href="Assets/css/indexstyle.css">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<header>
-    <nav>
-        <div class="logo">GalaxyPets</div>
-        <ul>
-            <li class="dropdown">
-                <a href="Pages/petprofile.php">Your Pet/Profile</a>
-                <div class="./dropdown-content">
-                    <a href="Pages/petprofile.php">My GalaxyPet</a>
-                    <a href="#">Closet</a>
-                    <a href="#">Settings</a>
-                </div>
-            </li>
-            <li class="dropdown">
-                Community
-                <div class="dropdown-content">
-                    <a href="Pages/clubs.html">Clubs</a>
-                    <a href="#">Users</a>
-                </div>
-            </li>
-            <li><a href="Pages/games.html">Games</a></li>
-            <li><a href="Pages/shop.html">Shop</a></li>
-        </ul>
-    </nav>
+
+<header class="border-bottom sticky-top">
+    <div id="navbar-container"></div>
 </header>
 
 <marquee behavior=scroll direction="left" scrollamount="5" style="color: #17ffee;">
@@ -38,88 +22,97 @@
 </marquee>
 
 <main>
-    <section class="newspaper">
-        <h2>GalaxyPets Daily News // Stardate 2025.03.10</h2>
-        <figure>
-            <img src="Assets/img/glorbus/green.PNG" alt="New Pets Added">
-            <figcaption>featured: green glorbus</figcaption>
-        </figure>
-        <h2>New Cosmic Creature: Glorbus is Here!</h2>
-        <p>We are thrilled to announce the arrival of the Glorbus, the galaxy's aloof and lovable little guy. These
-            charming creatures come in five vibrant colors: <b>light purple, blue, green, dark purple, and pink.</b>
-            Discovered roaming freely along the asteroid belt, we've carefully rounded them up for you to adopt and
-            cherish.</p>
-        <p>Glorbus is the perfect companion for those who like to lounge in their home and play video games - as he
-            likes to eat a lot!</p>
-        <p>We hope you'll form lasting bonds with these delightful creatures. Visit our adoption section to bring a
-            Glorbus home with you today and start your intergalactic friendship journey!</p>
-    </section>
-    <br>
-    <section class="newspaper">
-        <h2>GalaxyPets Daily News // Stardate 2025.03.09</h2>
-        <table>
-            <tr>
-                <td>
-                    <figure>
-                        <img src="Assets/img/planets/lavenderplanet.png" alt="planets">
-                        <figcaption>Atom's Halo</figcaption>
-                    </figure>
-                </td>
-                <td>
-                    <figure>
-                        <img src="Assets/img/planets/greenandpinkplanet.png" alt="planets">
-                        <figcaption>Galaxy Glitter</figcaption>
-                    </figure>
-                </td>
-                <td>
-                    <figure>
-                        <img src="Assets/img/planets/purpleplanet.png" alt="planets">
-                        <figcaption>Zero Gravity</figcaption>
-                    </figure>
-                </td>
-                <td>
-                    <figure>
-                        <img src="Assets/img/planets/blueplanet.png" alt="planets">
-                        <figcaption>Orbitors</figcaption>
-                    </figure>
-                </td>
-            </tr>
-        </table>
-        <h2>Explore and Socialize with Our New Clubs!</h2>
-        <p>We're excited to introduce a new way to connect with fellow space travelers and pet enthusiasts across the
-            galaxy! You can now join clubs and explore a variety of fascinating planets, each offering unique
-            experiences:</p>
-        <ul>
-            <li><b>Atom's Halo:</b> Explore the mysteries of the galactic halo, where stars and gas come together in a
-                celestial dance.
-            </li>
-            <li><b>Galaxy Glitter:</b> Immerse yourself in a world of sparkling cosmic dust and vibrant colors.</li>
-            <li><b>Zero Gravity:</b> Experience weightlessness and enjoy activities that defy gravity.</li>
-            <li><b>Orbitors:</b> Discover the thrill of orbiting celestial bodies and learn about their gravitational
-                secrets.
-            </li>
-        </ul>
-        <p>Visit these planets to meet new friends, share stories about your pets, and enjoy the wonders of the galaxy
-            together. Whether you're discussing the latest Stardust Jelly trends or sharing tips on cosmic pet care, our
-            clubs are the perfect place to connect and have fun! So, grab your space gear and start exploring today!</p>
-    </section>
-    </br>
-    <section class="newspaper">
-        <h2>GalaxyPets Daily News // Stardate 2025.03.09</h2>
-        <figure>
-            <img src="Assets/img/jelly/lightpurple.png" alt="New Pets Added">
-            <figcaption>featured: light purple stardust jelly</figcaption>
-        </figure>
-        <h2>New Cosmic Creature: The Stardust Jelly has arrived!</h2>
-        <p>We are thrilled to announce the arrival of the Stardust Jelly, a new and adorable addition to our galaxy of
-            pets! These charming creatures come in five vibrant colors: <b>light purple, blue, green, dark purple, and
-                pink.</b> Discovered roaming freely along the asteroid belt, we've carefully rounded them up for you to
-            adopt and cherish.</p>
-        <p>The Stardust Jelly is not only lovable but also eco-friendly, as they feed on cosmic dust, making them a
-            perfect companion for any planet or ecosystem. Their unique diet helps keep their surroundings clean,
-            ensuring a harmonious coexistence with other pets and environments.</p>
-        <p>We hope you'll form lasting bonds with these delightful creatures. Visit our adoption section to bring a
-            Stardust Jelly home today and start your intergalactic friendship journey!</p>
+    <section class="container article-container">
+        <section class="article">
+            <h2>GalaxyPets Daily News // Stardate 2025.03.10</h2>
+            <figure>
+                <img src="Assets/img/glorbus/green.PNG" alt="New Pets Added">
+                <figcaption>featured: green glorbus</figcaption>
+            </figure>
+            <h2>New Cosmic Creature: Glorbus is Here!</h2>
+            <p>We are thrilled to announce the arrival of the Glorbus, the galaxy's aloof and lovable little guy. These
+                charming creatures come in five vibrant colors: <b><span class="light-purple">light purple</span>,
+                    <span class="blue">blue</span>,
+                    <span class="green">green</span>,
+                    <span class="dark-purple">dark purple</span>
+                    , and <span class="pink">pink</span>.</b>
+                Dicovered roaming freely along the asteroid belt, we've carefully rounded them up for you to adopt and
+                cherish.</p>
+            <p>Glorbus is the perfect companion for those who like to lounge in their home and play video games - as he
+                likes to eat a lot!</p>
+            <p>We hope you'll form lasting bonds with these delightful creatures. Visit our adoption section to bring a
+                Glorbus home with you today and start your intergalactic friendship journey!</p>
+        </section>
+        <section class="article">
+            <h2>GalaxyPets Daily News // Stardate 2025.03.09</h2>
+            <div class="planet-grid">
+                <figure>
+                    <img src="Assets/img/planets/lavenderplanet.png" alt="planets">
+                    <figcaption>Atom's Halo</figcaption>
+                </figure>
+                <figure>
+                    <img src="Assets/img/planets/greenandpinkplanet.png" alt="planets">
+                    <figcaption>Galaxy Glitter</figcaption>
+                </figure>
+                <figure>
+                    <img src="Assets/img/planets/purpleplanet.png" alt="planets">
+                    <figcaption>Zero Gravity</figcaption>
+                </figure>
+                <figure>
+                    <img src="Assets/img/planets/blueplanet.png" alt="planets">
+                    <figcaption>Orbitors</figcaption>
+                </figure>
+            </div>
+            <h2>Explore and Socialize with Our New Clubs!</h2>
+            <p>We're excited to introduce a new way to connect with fellow space travelers and pet enthusiasts across
+                the
+                galaxy! You can now join clubs and explore a variety of fascinating planets, each offering unique
+                experiences:</p>
+            <ul>
+                <li><b class="pink">Atom's Halo:</b> Explore the mysteries of the galactic halo, where stars and gas
+                    come together in a celestial dance.
+                </li>
+                <li><b class="green">Galaxy Glitter:</b> Immerse yourself in a world of sparkling cosmic dust and
+                    vibrant colors.
+                </li>
+                <li><b class="dark-purple">Zero Gravity:</b> Experience weightlessness and enjoy activities that defy
+                    gravity.
+                </li>
+                <li><b>Orbitors:</b> Discover the thrill of orbiting celestial bodies and learn about their
+                    gravitational
+                    secrets.
+                </li>
+            </ul>
+            <p>Visit these planets to meet new friends, share stories about your pets, and enjoy the wonders of the
+                galaxy
+                together. Whether you're discussing the latest Stardust Jelly trends or sharing tips on cosmic pet care,
+                our
+                clubs are the perfect place to connect and have fun! So, grab your space gear and start exploring
+                today!</p>
+        </section>
+        <section class="article">
+            <h2>GalaxyPets Daily News // Stardate 2025.03.09</h2>
+            <figure>
+                <img src="Assets/img/jelly/lightpurple.png" alt="New Pets Added">
+                <figcaption>featured: light purple stardust jelly</figcaption>
+            </figure>
+            <h2>New Cosmic Creature: The Stardust Jelly has arrived!</h2>
+            <p>We are thrilled to announce the arrival of the Stardust Jelly, a new and adorable addition to our galaxy
+                of
+                pets! These charming creatures come in five vibrant colors: <b><span
+                            class="light-purple">light purple</span>,
+                    <span class="blue">blue</span>,
+                    <span class="green">green</span>,
+                    <span class="dark-purple">dark purple</span>
+                    , and <span class="pink">pink</span>.</b>
+                Discovered roaming freely along the asteroid belt, we've carefully rounded them up for you to
+                adopt and cherish.</p>
+            <p>The Stardust Jelly is not only lovable but also eco-friendly, as they feed on cosmic dust, making them a
+                perfect companion for any planet or ecosystem. Their unique diet helps keep their surroundings clean,
+                ensuring a harmonious coexistence with other pets and environments.</p>
+            <p>We hope you'll form lasting bonds with these delightful creatures. Visit our adoption section to bring a
+                Stardust Jelly home today and start your intergalactic friendship journey!</p>
+        </section>
     </section>
 </main>
 
@@ -127,12 +120,29 @@
     •°*”˜˜”*°•.ƸӜƷ.•°*”˜˜”*°•.ƸӜƷ•°*”˜˜”*°•.ƸӜƷ.•°*”˜˜”*°•.ƸӜƷ•°*”˜˜”*°•.ƸӜƷ
 </marquee>
 
-<footer>
-    <p>&copy; 2025 GalaxyPets</p>
-    <ul>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Official Site</a></li>
+<footer class="d-flex flex-wrap justify-content-between align-items-center border-top footer fixed-bottom">
+    <p class="col-md-4 mb-0 text-body-secondary">&copy; 2025 GalaxyPets</p>
+    <ul class="d-flex align-items-center list-unstyled m-0 ml-auto">
+        <li>
+            <a class="text-body-secondary"
+               href="README.md">About Us</a></li>
+        <li>
+            <a class="text-body-secondary"
+               href="#">Official Site</a></li>
     </ul>
 </footer>
+
+<script src="Assets/js/navbar.js"></script>
+
+<!-- Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
