5989c650b0bdbdcbe59a07fa3e54c58d6aec734a
Marina Atkin <marinaatkin@gmail.com>
2025-04-08 17:18:28 -0500

    fixed login so updates to latest timestamp

--- login.php
+++ login.php
@@ -86,14 +86,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
                             $_SESSION['username'] = $username;
 
-                            $sql2 = "IF EXISTS (SELECT * FROM latestLogin WHERE username = '".$username."') 
-										BEGIN
-										   UPDATE latestLogin WHERE username = '".$username."' 
-										END
-										ELSE
-										BEGIN
-										    INSERT INTO latestLogin (username) VALUES ('".$username."')
-										END";
+                            $sql2 = "INSERT INTO latestLogin (username) VALUES ('".$username."'') ON DUPLICATE KEY UPDATE date = CURRENT_TIMESTAMP";
 
 							if ($link->query($sql2) === TRUE) {
 								header("location: Pages/petprofile.php");
