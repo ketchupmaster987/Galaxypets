e7038ba9251615b1a23b0c39af7bad76b4486cfb
Marina Atkin <marinaatkin@gmail.com>
2025-04-10 15:58:07 -0500

    Update adopt.php

--- Pages/adopt.php
+++ Pages/adopt.php
@@ -52,7 +52,7 @@ session_start();
             
             <div id="pet">
             </div>
-            <form action="response.php" method="post">
+            <form action="login.php" method="post">
                 <fieldset>
                     <legend>Create Your Pet</legend>
                     <br>
@@ -102,22 +102,7 @@ session_start();
 </main>
 </div>
 
-<?php
-	
-	require_once '../config.php';
 
-	$username = $_SESSION['username'];
-	$species = $_POST['species'];
-	$color = $_POST['color'];
-	$planet = $_POST['planet']
-
-
-	$sql = "INSERT INTO pets (username, species, color, planet) VALUES ('$username', '$species', '$color', '$planet')";
-
-	mysqli_query($link,$sql);
-
-	
-?>
 
 </body>
 </html>
