e4228eb1c1b977960b1db35566defe8f52bfc737
Marina Atkin <marinaatkin@gmail.com>
2025-04-10 15:44:51 -0500

    Create adopt.php

---
+++ Pages/adopt.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<title>Adopt A Pet</title>
+<link rel="icon" href="icon.jpg" alt="website icon.jpg">
+<link rel="stylesheet" type= "text/css" href="../Assets/css/style.css">
+
+<meta http-equiv="Pragma" content="no-cache" />
+<?php
+    header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+?>
+</head>
+<?php 
+session_start();
+?>
+
+
+<body>
+
+<div>
+<header>
+    <?php
+
+    
+    if (!isset($_SESSION['username'])){
+        //echo "<script>alert('current user: ".$_SESSION['username']."')</script>";
+        header("location: /../login.php");
+    }
+    ?>
+    <nav>
+        <div class="logo"><a href="../index.php" style="text-decoration: none;">GalaxyPets</a></div>
+        <ul>
+            <li><a href="../index.php">Back to Home</a></li>
+            <li class="dropdown">
+                Community
+                <div class="dropdown-content">
+                    <a href="clubs.html">Clubs</a>
+                    <a href="#">Users</a>
+                </div>
+            </li>
+            <li><a href="games.html">Games</a></li>
+            <li><a href="shop.html">Shop</a></li>
+            <li><a href="/../logout.php">Log Out</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+	<br>
+	<div id="form">
+		<h2> Adopt a Pet </h2>
+        <div id="petPreview">
+            <div id="PreviewHeader">
+            Pet Preview:
+            </div>
+            <script type="text/javascript">
+
+                setInterval (function showPreview() {
+
+                    var species = document.getElementById('species').value;
+                    
+                    var color = document.getElementById('color').value;
+                    
+                    document.getElementById('pet').innerHTML = '<img src="../Assets/img/' + species + '/regular/' + color + '.png">'
+                    
+                    
+                }, 0.01 * 1000 );
+            </script>
+            <div id="pet">
+            </div>
+            
+        </div>
+			<form action="response.php" method="post">
+				<fieldset>
+					<legend>Create Your Pet</legend>
+                    <br>
+					Species:<br>
+                        <select name="species" id="species">
+						    <option value="glorbus">Glorbus</option>
+						    <option value="jelly">Jelly</option>
+                        </select>
+						<br>
+                        <br>
+					Color:<br>
+						<select name="color" id="color">
+                            
+                            <option value="green">Green</option>
+                            <option value="blue">Blue</option>
+                            <option value="lightpurple">Light Purple</option>
+                            <option value="darkpurple">Dark Purple</option>
+                            <option value="pink">Pink</option>
+                            
+                        </select>
+						<br>
+                        <br>
+					Planet:<br>
+                        <input type='text' value="planet">
+                        <br>
+                        <br>
+				</fieldset>
+				<br>
+				<?php 
+                if (isset($_SESSION['username'])){
+				    echo '<input type="submit" value="Adopt!">';
+                    echo ' ';
+				    echo '<input type="reset" value="Reset Form">';
+                } else {
+                    echo '<a href="../login.php">';
+                        echo '<div id="login-message">';
+                            echo "Log in to adopt a pet";
+                        echo '</div>';
+                    echo '</a>';
+                }
+                ?>
+			</form><br>
+            <!--Or, if you want to order for someone else <br> but don't know how they want their dragon, <br>why not get a <a href="orderGiftCard.php">gift card</a>?-->
+	</div>
+</main>
+</div>
+
+<?php
+	
+	require_once 'config.php';
+
+	$username = 'username';
+	$species = $_POST['species'];
+	$color = $_POST['color'];
+	$planet = $_POST['planet']
+
+
+	$sql = "INSERT INTO pets (username, species, color, planet) VALUES ('$username', '$species', '$color', '$planet')";
+
+	mysqli_query($link,$sql);
+
+	
+?>
+
+</body>
+</html>
