9d88d767bbba7b58cf61e163bec05bfbfa78ffc5
Marina Atkin <marinaatkin@gmail.com>
2025-04-17 15:52:31 -0500

    test

--- Pages/petprofile.php
+++ Pages/petprofile.php
@@ -38,7 +38,7 @@ session_start();
 
             //echo $row_date['prevDate'];
 
-            $time_away = date_diff($row_date['prevDate'], $row_date['date'])/86400;
+            $time_away = (date_diff(strtotime($row_date['prevDate']), strtotime($row_date['date'])))/86400;
             
             echo $time_away;
 
