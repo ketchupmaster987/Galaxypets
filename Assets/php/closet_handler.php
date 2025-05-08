cfc5223a527cae2f9651796c9aebd56ec6e74c1c
logan <Loganold@hotmail.com>
2025-05-08 16:39:13 -0500

    Fixed pathing? oops x4

--- Assets/php/closet_handler.php
+++ Assets/php/closet_handler.php
@@ -12,18 +12,18 @@ if (!isset($_SESSION['username'])) {
 $allItems = json_decode(file_get_contents("../json/items.json"), true);
 
 $accessories = getAccessories($link, "AliceFakeAcc");
-$accessoryNames = [];
+$accessoryIDs = [];
 
 if ($accessories && $accessories->num_rows > 0) {
     while ($row = $accessories->fetch_assoc()) {
-        $accessoryNames[] = $row; // $row will be: ['accessory' => 'Hat'], etc.
+        $accessoryIDs[] = $row['accessory']; // e.g., "hat-1", "hat-2"
     }
 }
 
 // Filter items to only those that match accessories
-//$matchedItems = array_filter($allItems, function ($item) use ($accessoryNames) {
-//    return in_array($item['name'], $accessoryNames);
-//});
+$matchedItems = array_filter($allItems, function ($item) use ($accessoryIDs) {
+    return in_array($item['id'], $accessoryIDs);
+});
 
 // We return the Accessories as Json
 header('Content-Type: application/json');
