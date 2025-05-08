bdc8953609e09bcf2e00e2fbe9c56f98b7d538d2
logan <Loganold@hotmail.com>
2025-05-08 16:40:38 -0500

    Fixed pathing? oops x5

--- Assets/php/closet_handler.php
+++ Assets/php/closet_handler.php
@@ -27,4 +27,4 @@ $matchedItems = array_filter($allItems, function ($item) use ($accessoryIDs) {
 
 // We return the Accessories as Json
 header('Content-Type: application/json');
-echo json_encode($accessoryNames);
+echo json_encode($matchedItems);
