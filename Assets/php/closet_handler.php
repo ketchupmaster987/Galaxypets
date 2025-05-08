cd1affebfd7f8f10a80b7bcb2dba0eb28564d913
logan <Loganold@hotmail.com>
2025-05-08 15:50:03 -0500

    Fixed pathing? oops x2

--- Assets/php/closet_handler.php
+++ Assets/php/closet_handler.php
@@ -9,9 +9,15 @@ if (!isset($_SESSION['username'])) {
     die("User not logged in");
 }
 
-header('Content-Type: application/json');
-
 $accessories = getAccessories($link, "AliceFakeAcc");
+$myData = [];
+
+if ($accessories && $accessories->num_rows > 0) {
+    while ($row = $accessories->fetch_assoc()) {
+        $myData[] = $row; // $row will be: ['accessory' => 'Hat'], etc.
+    }
+}
 
 // We return the Accessories as Json
-echo json_encode($accessories);
+header('Content-Type: application/json');
+echo json_encode($myData);
